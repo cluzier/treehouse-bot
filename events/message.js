a1f30a7508f05823a7b101ca1264ccd518f9444d
Conner Luzier <connerluzier@protonmail.com>
2019-07-03 00:27:30 -0400

    v1.1.0

--- events/message.js
+++ events/message.js
@@ -1,20 +1,10 @@
+const kick = require("../commands/kick");
+
 module.exports = (client, message) => {
-    if (message.content.startsWith('>kick')) {
-      const member = message.mentions.members.first()
-  
-      if (!member) {
-        return message.reply(
-          `Who are you trying to kick? You must mention a user.`
-        )
-      }
-  
-      if (!member.kickable) {
-        return message.reply(`I can't kick this user. Sorry!`)
-      }
-  
-      return member
-        .kick()
-        .then(() => message.reply(`${member.user.tag} was kicked.`))
-        .catch(error => message.reply(`Sorry, an error occured.`))
-    }
+  if (message.content.startsWith(">kick")) {
+    return kick(message);
   }
+  if (message.content === '>ping') {
+    message.reply('Pong!')
+  }
+};
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const fs = require('fs')
-fs.readdir('./events/', (err, files) => {
-    files.forEach(file => {
-      const eventHandler = require(`./events/${file}`)
-      const eventName = file.split('.')[0]
-      client.on(eventName, (...args) => eventHandler(client, ...args))
-    })
-  })
+require("dotenv").config();
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
 
-client.login('NTk1ODAwNjY4NzQ1NDk4NjU2.XRwR_A.P9fmhSUwyCgO57TrQuVdnPWVQdU')
+fs.readdir("./events/", (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`);
+    const eventName = file.split(".")[0];
+    client.on(eventName, (...args) => eventHandler(client, ...args));
+  });
+});
+
+client.login(process.env.BOT_TOKEN)
