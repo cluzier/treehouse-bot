6d88e2af733c1dba8fc52c1e3d216870746c5126
Conner Luzier <connerluzier@protonmail.com>
2019-07-04 19:08:21 -0400

    nitro boosters

--- commands/info.js
+++ commands/info.js
@@ -37,8 +37,8 @@ exports.run = (client, message, args) => {
     `${human.date('m-d-y | h:i:s', client.readyAt)} ${amPm()}`, true)
   .addField("Memory Usage",
     `Using ${bytes(process.memoryUsage().rss)}`, true)
-  .addField("Shoutouts",
-    "Chris Hansenᶠʳᵒᵐ ᴰᵃᵗᵉˡⁱⁿᵉ ᴺᴮᶜ#2020 \n ayla#0001 \n Sely 🍑🇵🇷‼#1094 \n Corazon#7777 \n loserface#9476", true)
+  .addField("Nitro Boosters",
+    "Chris Hansenᶠʳᵒᵐ ᴰᵃᵗᵉˡⁱⁿᵉ ᴺᴮᶜ#2020 \n Krymena#0001 \n 𝒥𝑒𝓁𝓁𝒾~#0666  \n MustardPie#9999 \n tyler#7157 \n StreetCorner#9082 \n stickilas#1188 \n ada#0690 \n ZkyliHokageXX#7015 \n Distilled Delusion#2494", true)
   .addField("Total Guilds Serving",
     `${client.guilds.size}`, true)
   .setTimestamp()
