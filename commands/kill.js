09c89cba09c3b878171372ec1508b9fea9bd4a45
Conner Luzier <connerluzier@protonmail.com>
2019-07-21 14:49:05 -0400

    ./

--- commands/kill.js
+++ commands/kill.js
@@ -3,29 +3,42 @@ const fs = require('fs');
 const kills = require('../killed.json');
 
 exports.run = (client, message, args) => {
-    const target = message.mentions.users.first();
+  const target = message.mentions.users.first();
 
-    if (!target)
-      return message.channel.send(`${message.author} killed themselves. 💀`);
+  if (!target)
+    return message.channel.send(`${message.author} killed themselves. 💀`);
 
-    const id = target.id;
-    let deathCount = kills[id];
+  const id = target.id;
+  let deathCount = kills[id];
 
-    if (!deathCount) {
-      kills[id] = 1;
-      message.channel.send(`${message.author} killed ${target.tag} 🔪`)
-      message.channel.send(`${target.tag} has been killed for the first time!`);
-    } else {
-      deathCount = (kills[id] = kills[id] + 1);
-      message.channel.send(`${message.author} killed ${target.tag} 🔪`)
-      message.channel.send(`${target.tag} has been killed for the ${deathCount} times!`);
-    }
+  if (!deathCount) {
+    kills[id] = 1;
 
-    // Update kills file
-    fs.writeFileSync(
-      "../killed.json",
-      JSON.stringify(kills),
-      (err) => console.log(err),
-      console.log(`${message.author} killed ${target.tag} 🔪`)
-    );
+    const emb = new Discord.RichEmbed()
+      .setColor('#0099ff')
+      .addField(`${message.author} killed ${target.tag} 🔪`, `${target.tag} has been killed for the first time!`, true)
+      .setImage('https://i.imgur.com/7MkzxTT.gif')
+
+    message.channel.send(emb);
+    // message.channel.send(`${message.author} killed ${target.tag} 🔪`)
+    // message.channel.send(`${target.tag} has been killed for the first time!`);
+  } else {
+    deathCount = (kills[id] = kills[id] + 1);
+
+    const emb = new Discord.RichEmbed()
+      .setColor('#0099ff')
+      .addField(`${message.author} killed ${target.tag} 🔪`, `${target.tag} has been killed ${deathCount} times!`, true)
+      .setImage('https://i.imgur.com/7MkzxTT.gif')
+
+    message.channel.send(emb);
+    // message.channel.send(`${message.author} killed ${target.tag} 🔪`)
+    // message.channel.send(`${target.tag} has been killed ${deathCount} times!`);
   }
+
+  // Update kills file
+  fs.writeFileSync(
+    "./killed.json",
+    JSON.stringify(kills),
+    (err) => console.log(err)
+  );
+}
